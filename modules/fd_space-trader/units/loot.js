4b18823b83f40cb1a6c7e6293225092f6e40be1d
Frank Dreitzner <frank.dreitzne@gmail.comr>
2018-06-19 19:34:33 +0200

    nachtrag

---
+++ modules/fd_space-trader/units/loot.js
@@ -0,0 +1,3 @@
+// TODO ausgelagerte Classe für Loot / Mining Items
+// TODO Spawn random wenn Asteorids destroyd
+// TODO Kill wenn Player over Loot und geringe Speed differenz und z.B. Key "E" ????
---
+++ modules/fd_space-trader/units/station.js
@@ -0,0 +1,3 @@
+// TODO ausgelagerte Classe für Stationen
+// TODO Spawn in der nähe von Spieler  und auschechen auf UNIVERSE Object Holder
+// TODO Kill wenn nicht mehr in der Nähe und wieder in UNIVERSE Object Holder einchecken
